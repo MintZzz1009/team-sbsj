8e115f2148e1ce994b22435cbe5ee88442e19fde
SangWooRyu <rookas33@gmail.com>
2022-12-09 02:27:33 +0900

    표시 수정

--- static/JS/posting.js
+++ static/JS/posting.js
@@ -27,7 +27,7 @@ function show_all_posting() {
 
                 let user_name = rows[i][11]
                 let user_email = rows[i][12]
-                let user_desc = rows[i][10]
+                let user_desc = rows[i][13]
                 let user_profile_img_src = rows[i][15]
                 let showpostbox =
                     new PostingBox(
